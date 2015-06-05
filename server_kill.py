9b01730abf5aa048678e52090806246473623b4e
studenteternal <js1490@gmail.com>
2015-06-05 17:11:24 -0500

    changed file names for consitant paridigm across project, included project plan int repo

---
+++ server_kill.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import SoftLayer
+import yaml
+
+
+creds_file = open("softcreds.yaml",'r')
+creds = yaml.load(creds_file)
+
+client = SoftLayer.Client(username=(creds['username']), api_key=(creds['api_key']))
+
+FH = open('kill-file','r')
+
+kill_servers = FH.read().splitlines()
+
+for server in kill_servers:
+	print "Killing " + server
+	client['Virtual_Guest'].deleteObject(id=server)
+
+FH.close()
