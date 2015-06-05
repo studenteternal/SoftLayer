e565f0489d5072599970caedb0ba197708a60a1b
studenteternal <js1490@gmail.com>
2015-06-05 13:04:57 -0500

    added toms changes

--- serverkill.py
+++ serverkill.py
@@ -1,21 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import SoftLayer
 import yaml
 
-credsFile = open("softcreds.yaml",'r')
-creds = yaml.load(credsFile)
 
-
+creds_file = open("softcreds.yaml",'r')
+creds = yaml.load(creds_file)
 
 client = SoftLayer.Client(username=(creds['username']), api_key=(creds['api_key']))
 
-f = open('kill-file','r')
+FH = open('kill-file','r')
 
-for line in f:
-	
-	s = f.readline()
-	client['Virtual_Guest'].deleteObject(s)
+kill_servers = FH.read().splitlines()
 
-close('kill-file')
+for server in kill_servers:
+	print "Killing " + server
+	client['Virtual_Guest'].deleteObject(server)
 
+FH.close()
