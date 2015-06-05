6c563859deed21504aa1a90fba3102e0819cff8c
studenteternal <js1490@gmail.com>
2015-06-05 12:24:49 -0500

    updated server remove file

--- serverkill.py
+++ serverkill.py
@@ -10,10 +10,12 @@ creds = yaml.load(credsFile)
 
 client = SoftLayer.Client(username=(creds['username']), api_key=(creds['api_key']))
 
-f = open('kill-file',' r')
+f = open('kill-file','r')
+
+for line in f:
 	
-	 
-	client['Virtual_Guest'].deleteObject(i)
+	s = f.readline()
+	client['Virtual_Guest'].deleteObject(s)
 
 close('kill-file')
 
