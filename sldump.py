3238a5f343393568c7463e17c4ebabbe268ba5c0
Jeff Sampson <js1490@gmail.com>
2017-06-15 18:59:25 +0000

    added basic functality to sldump

--- sldump.py
+++ sldump.py
@@ -3,8 +3,11 @@
 import yaml
 import SoftLayer
 from pprint import pprint 
+import datetime
+from datetime import datetime
+from datetime import date
 
-#ilehandlers to load account specific information
+#filehandlers to load account specific information
 credsFile = open("softcreds.yaml",'r')
 creds = yaml.load(credsFile)
 
@@ -12,116 +15,217 @@ creds = yaml.load(credsFile)
 #print creds['api_key']
 
 # variable declerations
-load = {}
-servers = []
 
+load = {}
+vservers = []
+errorlist = {}
+accountitems = []
 
 client = SoftLayer.Client(username=(creds['username']), api_key=(creds['api_key']))
 
-n = 1
-count = 1
-
-# this line works, however there is an issue with the sub-filters not filtering.  
-# preserving this line for education and refrence 
-#filtermask = "mask[hostName, id, orderItem[order[userRecord[email]]]]"
-
-filtermask = "mask[hostname, id]"
-
-#nameandid1 = []
-
-# this line is giving all the billing items for the month, including canceled devices
-#  getemailaddress = client['SoftLayer_Account'].getNextInvoiceTopLevelBillingItems(mask = filtermask)
-#print getemailaddress
-n = 0 
-y = ""
-
-# thi is to test limiting the call with offest
+# this section loads the billing data for the current month
+# -----------------------------------------------------------------------------------
 
 
-#n = 0
-#print type(getemailaddress)
-#getemailaddress = {1}
-#stop = {}
-#while ( n < 1 ):
-#	stop = getemailaddress
-getemailaddress = client['SoftLayer_Account'].getNextInvoiceTopLevelBillingItems (mask = filtermask, id = 27950833)
-print getemailaddress
-#	print n
-#	n = n + 1
+filtermask = "mask[orderItem[order[userRecord[email]]]]"
+getemailaddress = client['SoftLayer_Account'].getNextInvoiceTopLevelBillingItems(mask = filtermask)
 
-# this works! 
-#while (y != 'jbsampso-adnpub-bastion1'):
- #		x = {}
-# 		x = getemailaddress[n]
-# 		y = x['hostName']
-# 		n = n + 1
-# 	#	print x
-# 		print x['orderItem']
-# 		z = x['orderItem']
-# 		print type(z)
-# 		print  z['order']
-# 		a = z['order']
-# 		b = a['userRecord']
-# 		print b
-# 		c = a['userRecord']
-# 		print c['email']
-# 	
-# 	#	print z['userRecord']
-# 	
-# 	#	print z['userRecord['email']']	
+# this section loads the active Virtual server list
+# ------------------------------------------------------------------------------------
 
 
-# Virtual servers - seperate sections will be needed for baremetal servers and other billing items. 
 
-n = 0
-m = 1
-while (n < 6):
-	nameandid1 = client['SoftLayer_Account'].getVirtualGuests(mask = filtermask, offset = 3)
-		
-	print nameandid1[n]
-	print n
-	n = n + 1
-	m = m + 1
-# print type(nameandid1)
+filtermask = "mask[hostname, id, provisionDate]"
 
+nameandid1 = client['SoftLayer_Account'].getVirtualGuests(mask = filtermask)
+
+#print type(nameandid1)
 #print nameandid1[0]
+vslength = len(nameandid1)
 
-#temp = nameandid1[0]
-#x = 0
-#end = len(nameandid1)
+#print vslength
+#print type(nameandid1)
 
-#while (x < end): 
-#	temp = nameandid1[x]
-#	print temp['id']
-#	temp2 = client['SoftLayer_Account'].getObject(temp['id'])
-#	y = 0
-#	z = 0
-#	while (y != temp['id']):
-#		onebillingitem = getemailaddress[z]
-#		y = onebillingitem['id']
-#		print y
-#		print temp['id']
-#		z = z + 1
-#	print temp2
-#	load = {'id' : temp['id'], 'hostname' : temp['hostname'], 'email' : temp2['email'], 'modifyDate' : temp2['modifyDate']}
-#	print load
+# this loop associates the e-mail address and user to each virtual server for reporting.
+# set loop counters
+n = 0 
+#t = vslength - 1
+# seems to consistantly be missing the last virtual server when I compare results to verifiy so the -1 my be a mistake
+t = vslength
+m = 0
+y = 0
+cnt = 0
+id = 0
+error = 0
+errorcount = 0
+while (n < t):
+	srv = nameandid1[n]	
+	n = n + 1
+	cnt = srv['id']
+	m = 0
+	while (y != cnt):
+		try:
+			x = getemailaddress[m]
+#			print srv['id']
+			y = x['resourceTableId']
+			id = y
+			m = m + 1
+		except:
+			errorlist[errorcount] = srv
+			y = cnt
+			print ("adding server to error file", srv)
+			error = 1
+			errorcount = errorcount + 1
+	if (error == 0):
+		a = x['orderItem']
+		b = a['order']
+		c = b['userRecord']
+		d = c['email']
+		srv['email'] = d
+		vservers.append(srv)
+		accountitems.append(srv)
+	else:
+		error = 0
+
+#print vservers
+#print "servers with errors"
+#print errorlist
 #	servers.append(load)  	
-#	x = x + 1
-		
-#	print load
 
-#print  client['SoftLayer_Account'].getObject(28668677)
-
-#print servers
+#print errorlist
 
 
-#print len(nameandid1)
+# this section loads the active Physical server list
+# ------------------------------------------------------------------------------------
+
+filtermask = "mask[hostname, id, provisionDate]"
+
+nameandid2 = client['SoftLayer_Account'].getHardware(mask = filtermask)
+print 'this should be the baremetal servers'
+print type(nameandid2)
+print nameandid2
+
+hwlength = len(nameandid2)
+
+# list to associate e-mail address to bare metal server orders
+# initialize loop variables 
+n = 0 
+t = hwlength
+m = 0
+y = 0
+cnt = 0
+id = 0
+error = 0
+
+while (n != t):
+	srv = nameandid2[n]	
+	n = n + 1
+	cnt = srv['id']
+	m = 0
+	while (y != cnt):
+		try:
+			x = getemailaddress[m]
+#			print srv['id']
+			y = x['resourceTableId']
+			id = y
+			m = m + 1
+		except:
+			errorlist[errorcount] = srv
+			print 'adding bare metal server to error list'
+			y = cnt
+			error = 1
+			errorcount = errorcount + 1
+	if (error == 0):
+		a = x['orderItem']
+		b = a['order']
+		c = b['userRecord']
+		d = c['email']
+		srv['email'] = d
+#		print (srv)
+		accountitems.append(srv)
+	else:
+		error = 0
+
+#print 'baremetal servers with errors'
+#print errorlist
+ 
+# this section load the active Neworking resources 
+# ------------------------------------------------------------------------------------
+
+# this section loads any other billing items on the account
+# ------------------------------------------------------------------------------------
+
+# This section simply appends all the item types into a master list of billing items
+# ------------------------------------------------------------------------------------
+
+#accountitems.append(vservers)
+
+length = len(accountitems)
+length = length 
+
+#print length
+#print 'this is the account list'
+#print accountitems
+
+# this section identifies hardware more then 7 days old and creates a new list of potential policy violations
+# ------------------------------------------------------------------------------------
+
+today = date.today()
+#print today
+#x = vservers[0]
+#print x
+#servertime = x['provisionDate']
 
 
-#print client['SoftLayer_Account'].getObject('156844963')
+#print servertime
+#print type(servertime)
+#servertime = servertime[:10]
+#print servertime
+
+#servertime = datetime.strptime(servertime, '%Y-%m-%d')
+#servertime = datetime.date(servertime)
+#print servertime
+
+today = date.today()
+#print today
+
+#delta = today - servertime
+#print delta.days
+
+#intalize loop variables 
+t = length 
+n = 0
+error == 0
+while (n != t):
+	try:
+		item = accountitems[n]
+		itemtime = item['provisionDate']
+		itemtime = itemtime[:10]
+		itemtime = datetime.strptime(itemtime, '%Y-%m-%d')
+		itemtime = datetime.date(itemtime)
+		delta = today - itemtime
+#		print item
+		if delta.days > 7:
+			print item
+#			wastetime = wastetime + 1
+	except:
+		errorlist[errorcount] = item
+		y = n
+		error = 1
+		errorcount = errorcount + 1
+	if (error == 0):
+		n = n + 1
+	else:
+		error = 0
+		n = n + 1
+
+print 'server that had errors'
+print errorlist
+
+# this section removes any imortal items
+# ---------------------------------------------------------------------------------------------
+
+
 
 credsFile.close()
 
-
-#pprint( server_return )
-#print server_return['id']
